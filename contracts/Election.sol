cfe1d0397975453e14c2d13ce40f91e84c9ac8ac
a-colabella <acolabella11@gmail.com>
2020-09-20 21:14:23 -0700

    tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,29 @@
 pragma solidity >= 0.4.11 < 0.8.0;
 
 contract Election {
-  // Store candidate
-  // Read candidate
-  string public candidate;
+  // Model a candidate
+  struct Candidate {
+    uint id;
+    string name;
+    uint votesCount;
+  }
+
+  // Store a candidates
+
+  // Fetch Candidate
+  mapping(uint => Candidate) public candidates;
+
+  // Store candidates Count
+  uint public candidatesCount;
 
   // Constructor
   constructor () public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate (string memory _name) private {
+    candidatesCount++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
